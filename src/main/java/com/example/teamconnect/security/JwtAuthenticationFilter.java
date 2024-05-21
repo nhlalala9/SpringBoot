014f38081238946082affab3b2000608f013dbc7
Nhlalala <nhlalala60@gmail.com>
2024-05-21 14:09:41 +0200

    first commit

--- src/main/java/com/example/teamconnect/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/teamconnect/security/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
 
